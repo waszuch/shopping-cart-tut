b73b3d0b6969c84939c13490f89448cb89f7f8a7
waszuch <140809966+waszuch@users.noreply.github.com>
2024-08-23 12:33:58 +0200

    Finish
    ...

--- shopping.cart/src/App.tsx
+++ shopping.cart/src/App.tsx
@@ -4,10 +4,11 @@ import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
 import { About } from "./pages/About"
 import { Navbar } from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContex"
 
 function App() {
 return (
-<>
+  <ShoppingCartProvider>
 <Navbar />
 <Container className="mb-4">
   <Routes>
@@ -16,7 +17,8 @@ return (
     <Route path="/about" element={<About />} />
   </Routes>
 </Container>
-</>
+</ShoppingCartProvider>
+
 )
 }
 
--- shopping.cart/src/components/StoreItem.tsx
+++ shopping.cart/src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import { Button, Card } from "react-bootstrap"
 import { formatCurrency } from "../utilities/formtaCurrency"
+import { useShoppingCart } from "../context/ShoppingCartContex"
 
 type StoreItemProps = {
     id: number
@@ -9,7 +10,14 @@ type StoreItemProps = {
 }
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps){
-    const quantity = 0
+   const {
+    getItemQuantity, 
+    increaseCartQuantity, 
+    decreaseCartQuantity, 
+    removeFromCart
+} = useShoppingCart()
+    const quantity = getItemQuantity(id)
+
     return <Card className="h-100">
         <Card.Img variant="top" 
         src={imgUrl} 
@@ -24,17 +32,17 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps){
         </Card.Title>
         <div className="mt-auto">
         {quantity === 0 ? (
-            <Button className="w-100">+ Add To Cart</Button>
+            <Button className="w-100" onClick={()=> increaseCartQuantity(id) }>+ Add To Cart</Button>
         ):(
         <div className="d-flex align-items-center flex-column" style={{gap: ".5rem"}}>
             <div className="d-flex align-item-center justify-conteny-center " style={{gap: ".5rem"}}>
-            <Button>-</Button>
+            <Button onClick={()=> decreaseCartQuantity(id) }>-</Button>
            <div>
             <span className="fs-3">{quantity}</span> in cart
             </div>
-            <Button>+</Button>
+            <Button onClick={()=> increaseCartQuantity(id) }>+</Button>
             </div>
-            <Button variant="danger" size="sm">
+            <Button variant="danger" size="sm" onClick={()=> removeFromCart(id) }>
                 Remove
                 </Button>
             </div>
